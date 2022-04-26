d4bedee2083a37057e2aff1ca8df69c30f6e2508
sua <72644361+sssua-0928@users.noreply.github.com>
2022-04-26 21:00:18 +0900

    파일 기본세팅

---
+++ mid_assignment/mid_assignment/game_play.cpp
@@ -0,0 +1,5 @@
+#include "main.h"
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <atlstr.h>
---
+++ mid_assignment/mid_assignment/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+void InitGame();
+void HandleEvents();
+void Update();
+void Render();
+void ClearGame();
+
+extern bool g_flag_running;
+
+
+
